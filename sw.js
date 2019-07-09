d582a2a005c326c79509970dbe73e3f2e93bf467
Sara Eisa <prosarahgamal@outlook.com>
2019-07-10 00:44:25 +0200

    add images links to cache

--- sw.js
+++ sw.js
@@ -3,11 +3,23 @@ self.addEventListener('install', function(event) {
     caches.open('restaurant-reviews-static-v1').then(function(cache) {
       return cache.addAll([
         '/',
-        'js/main.js',
-        'css/styles.css',
-        'js/dbhelper.js',
-        'js/restaurant_info.js',
-        'data/restaurants.json'
+        './index.html',
+        './restaurant.html',
+        './css/styles.css',
+        './js/dbhelper.js',
+        './js/main.js',
+        './js/restaurant_info.js',
+        './data/restaurants.json',
+        './img/1.jpg',
+        './img/2.jpg',
+        './img/3.jpg',
+        './img/4.jpg',
+        './img/5.jpg',
+        './img/6.jpg',
+        './img/7.jpg',
+        './img/8.jpg',
+        './img/9.jpg',
+        './img/10.jpg'
       ]);
     })
   );
