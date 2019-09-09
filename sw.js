705f70eebd71d2eccef7ac3865f4e1aecb108101
Sara Eisa <prosarahgamal@outlook.com>
2019-09-09 19:48:34 +0200

    connect to db

--- sw.js
+++ sw.js
@@ -1,4 +1,15 @@
-var staticCacheName = 'restaurant-reviews-static-v3';
+importScripts('js/idb.js');
+
+const staticCacheName = 'restaurant-reviews-static-v13';
+const contentImgsCache = 'restaurant-reviews-content-imgs';
+
+const openDatabase = () => {
+  return idb.open('restaurant-reviews', 1, (upgradeDb) => {
+    const store = upgradeDb.createObjectStore('restaurants', {
+      keyPath: 'id'
+    });
+  });
+};
 
 self.addEventListener('install', function (event) {
   event.waitUntil(
@@ -11,18 +22,13 @@ self.addEventListener('install', function (event) {
         './js/dbhelper.js',
         './js/main.js',
         './js/restaurant_info.js',
-        './data/restaurants.json',
-        './img/1.jpg',
-        './img/2.jpg',
-        './img/3.jpg',
-        './img/4.jpg',
-        './img/5.jpg',
-        './img/6.jpg',
-        './img/7.jpg',
-        './img/8.jpg',
-        './img/9.jpg',
-        './img/10.jpg',
+        './favicon.ico',
+        'https://cdn.rawgit.com/mozilla/localForage/master/dist/localforage.js',
+        '/js/idb.js',
+        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.css',
+        'https://unpkg.com/leaflet@1.3.1/dist/leaflet.js'
       ]))
+      .then(() => self.skipWaiting())
   );
 });
 
@@ -31,13 +37,92 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
+  const requestUrl = new URL(event.request.url);
+  if (requestUrl.pathname.startsWith('/img-res/')) {
+    event.respondWith(servePhoto(event.request));
+    return;
+  }
+
+  if (requestUrl.pathname.startsWith('/restaurants')) {
+    event.respondWith(serveRestaurants(event.request));
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request, { ignoreSearch: true }).then(response => {
-      if(response) return response;
-      return fetch(event.request).then(function(res) {
-        cache.put(event.request, res.clone());
-        return res;
-      });
+      if (response) return response;
+      return fetch(event.request).then(function (networkRes) {
+        return networkRes;
+      })
+      .catch(err => console.log('error ' + err));
     }).catch(err => console.log(err))
   );
+});
+
+const servePhoto = (request) => {
+  const storageUrl = request.url.replace(/large\/|medium\/|small\//, '');
+  return caches.open(contentImgsCache)
+    .then(cache => {
+      return cache.match(storageUrl)
+        .then(res => {
+          if (res) return res;
+          return fetch(request)
+            .then(networkRes => {
+              cache.put(storageUrl, networkRes.clone());
+              return networkRes;
+            })
+        })
+    });
+}
+
+const serveRestaurants = (request) => {
+  const dbPromise = openDatabase();
+    return dbPromise.then(db => {
+      if (!db) return;
+      const tx = db.transaction('restaurants');
+      const restaurantStore = tx.objectStore('restaurants');
+      return restaurantStore.getAll();
+    })
+    .then(restaurants => {
+      if(restaurants.length > 0){
+        let response = new Response(JSON.stringify(restaurants), {
+          headers: new Headers({
+            'Content-type': 'application/json'
+          }),
+          status: 200
+        });
+        return response;
+      }
+      return fetch(request)
+      .then(res => res.json())
+      .then(networkRes => {
+        dbPromise.then(db => {
+          const tx = db.transaction('restaurants', 'readwrite');
+          const restaurantStore = tx.objectStore('restaurants');
+          networkRes.forEach(o => restaurantStore.put(o));
+          return tx.complete;
+        })
+        .then(() => {
+          const blob = new Blob(networkRes, {type : 'application/json'});
+          const init = { "status" : 200 , "statusText" : "SuperSmashingGreat!" };
+          const response = new Response(blob, init);
+          return response;
+        })
+        .catch(err => {
+          console.log('error while adding to db ' + err);
+        })
+      })
+      .catch(err => {
+        console.log('error while fetching data from network ' + err);
+      })
+    })
+    .catch(err => {
+      console.log('error while fetching data from db ' + err);
+    })
+}
+
+self.addEventListener('message', function (event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
 });
